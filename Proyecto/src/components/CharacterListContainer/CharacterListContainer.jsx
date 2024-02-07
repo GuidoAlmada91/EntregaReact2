474b63a0edfe3c55e27323b0a98e59cb7fe1bbd2
Guido Almada <guido.almada.91@gmail.com>
2024-02-07 16:00:53 -0300

    Mapeando

--- Proyecto/src/components/CharacterListContainer/CharacterListContainer.jsx
+++ Proyecto/src/components/CharacterListContainer/CharacterListContainer.jsx
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
+import { Character } from "../Character/Character";
 
 export const CharacterListContainer = () => {
+
+  const [characters, setcharacters] = useState([])
   
   const getCharacters = async () =>{
+
+  
     const resp = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
     const data = await resp.json();
-    console.log(data)
-      
+    setcharacters(data.results)
+    
+    
   }
 
-  getCharacters()
+  useEffect(() => {
+    getCharacters ();
+  
+  }, []);
   
   return (
 
-    <div>
-      CharacterListContainer
-    </div>
+    <>
+      {characters.map(character => <p key={character.id}> Nombre:  {character.name}</p>)}
+    </>
   )
 }
